761d9b75eae1242c06c0c7b3f0e7ab901af5c47a
tadanoluka <simplygod39@gmail.com>
2022-10-25 21:41:01 +0200

    Stage 1/5

--- Cinema.kt
+++ Cinema.kt
@@ -1,5 +1,40 @@
 package cinema
 
+class CinemaGrid {
+    val row = 7
+    val column = 8
+    val grid = mutableListOf(mutableListOf<String>())
+
+    init {
+        val firstRow = mutableListOf<String>(" ")
+        for (i in 1..column) {
+            firstRow.add(i.toString())
+        }
+        grid.add(firstRow)
+        for (i in 1..row) {
+            val rowList = mutableListOf<String>(i.toString())
+            repeat (column) {
+                rowList.add("S")
+            }
+            grid.add(rowList)
+        }
+        grid.removeAt(0)
+    }
+}
+
+class CinemaManager {
+    val cinemaGrid = CinemaGrid()
+
+    fun printGrid() {
+        println("Cinema:")
+        for (i in 0..cinemaGrid.row) {
+            println(cinemaGrid.grid[i].joinToString(" "))
+        }
+
+    }
+}
+
 fun main() {
-    // write your code here
+    val cinemaManager = CinemaManager()
+    cinemaManager.printGrid()
 }
